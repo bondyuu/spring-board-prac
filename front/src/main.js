d40656fef8503cbf3dd18c820fbde59b7be29478
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-03-02 16:42:11 +0900

    Update main.js

--- front/src/main.js
+++ front/src/main.js
@@ -1,11 +1,5 @@
+import { createApp } from 'vue'
 import App from './App.vue'
-import Vue from 'vue'
-import router from './router/index.js'
+import router from './router'
 
-Vue.config.productionTip = false
- 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
-
+createApp(App).use(router).mount('#app')
