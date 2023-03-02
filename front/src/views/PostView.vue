f6051d667e5ebd00cc695e44f3eccd0982b470f6
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-03-02 17:18:17 +0900

    vuex setting

---
+++ front/src/views/PostView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Vuex Test</h1>
+        {{ counter }} <br>
+        {{ $store.getters.getTwoPowerCounter }}<br>
+        <button @click="increase">increase</button>
+    </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+export default {
+    setup() {
+        const store = useStore();
+        const counter = computed(() => store.state.counter);
+        const increase = () => store.commit('setCounter', ++counter.value);
+
+        return {counter, increase}
+    }
+}
+</script>
