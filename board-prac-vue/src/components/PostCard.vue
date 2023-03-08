1f655206587d49a77ac4e436c088a89280cf7ce3
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-03-08 15:06:06 +0900

    검색 기능을 위한 컴포넌트 수정

---
+++ board-prac-vue/src/components/PostCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <b-card >
+        <b-card-title class="title">{{ item.title }}</b-card-title>
+        <b-card-text class="content">
+            {{ item.content }}
+        </b-card-text>
+        <b-card-text class="small text-muted">{{ item.user.email }}</b-card-text>
+    </b-card>
+</template>
+  
+<script>
+
+export default {
+    name: 'PostCard',
+    props: {
+        item: Object
+    }
+}
+</script>
+
+<style scoped>
+/* .wrapper {
+    margin-bottom: 20px; 
+    margin-right: 3%;
+    width: 32%;
+    display: inline-block;
+} */
+.title, .content {
+    text-align: left;
+    margin-left: 2%;
+    margin-top: 2%;
+}
+
+.small {
+    text-align: right;
+}
+</style>
--- board-prac-vue/src/views/PostListView.vue
+++ board-prac-vue/src/views/PostListView.vue
@@ -1,29 +1,66 @@
 <template>
   <div class="search-form">
     <b-nav-form>
-      <b-form-input class="mr-sm-2" placeholder="Search"></b-form-input>
-      <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Search</b-button>
+      <b-form-input class="mr-sm-2" placeholder="Search" v-model="searchTitle"></b-form-input>
+      <b-button variant="outline-success" class="my-2 my-sm-0" @click="getBoards">Search</b-button>
       <b-button variant="outline-success" class="bt-write" href="/write-post">New Post</b-button>
     </b-nav-form>
   </div>
   <div class="postlist">
-    <PostList />
+    <div class="wrapper" v-for="(obj, idx) in list" :key="idx">
+      <PostCard :item="obj"/>
+    </div>
   </div>
 </template>
 
 <script>
-import PostList from '@/components/PostList.vue'
+import PostCard from '@/components/PostCard.vue'
+import axios from 'axios'
+
 export default {
   name: 'PostListView',
   components: {
-    PostList
+    PostCard
+  },
+  data() {
+    return {
+        list: [],
+        searchTitle: ''
+    };
+  },
+  mounted() {
+      this.getBoards()
+  },
+  methods: {
+    getBoards() {
+      console.log(this.searchTitle);
+      axios
+      .get('http://localhost:8080/posts?title='+this.searchTitle,
+      {
+          headers: {
+              'Authorization': this.$store.state.accessToken
+          }
+      })
+      .then((res) => {
+          this.list = res.data;
+      })
+      .catch((err) => {
+          console.log(err);
+      });
+    }
   }
 }
 </script>
 
 <style scoped>
-.postlist {
+.postlist{
   text-align: center;
+}
+.wrapper {
+  margin-bottom: 20px; 
+  margin-right: 3%;
+  width: 32%;
+  display: inline-block;
 }
 .mr-sm-2 {
   width: 30%;
