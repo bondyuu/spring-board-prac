4b58bc12ef99b915d426ad58ed2488e5c26d66b5
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-03-10 17:44:48 +0900

    어드민 - 유저 삭제 기능 추가

--- board-prac-vue/src/views/admin/AdminUsersView.vue
+++ board-prac-vue/src/views/admin/AdminUsersView.vue
@@ -3,12 +3,23 @@
     <div class="search-form">
         <b-nav-form>
             <b-form-input class="mr-sm-2" placeholder="Search" v-model="email"></b-form-input>
-            <b-button variant="outline-success" class="my-2 my-sm-0" @click="getMain">Search</b-button>
+            <b-button variant="outline-success" class="my-2 my-sm-0" @click="getUsers">Search</b-button>
         </b-nav-form>
     </div>
     <div class="tbl-wrapper">
-    <b-table striped hover :items="list"></b-table>
-  </div>
+        <b-table :fields="fields" :items="list">
+            <template #cell(key)="data">
+                {{ data.index + 1 }}
+            </template>
+            <template #cell(email)="data">
+                <a href="#"> {{ data.value }} </a>
+            </template>
+            <template #cell(action)="data">
+                <b-button v-if="data.item.status==='활성회원'" variant="outline-success" class="btn-del" @:click="deleteUser(data.item.id)">Delete</b-button>
+                <!-- deleteUser(data.item.id) -->
+            </template>
+        </b-table>
+    </div>
 </template>
 
 <script>
@@ -17,18 +28,23 @@ import axios from 'axios'
 export default {
     name: 'AdminMainView',
     mounted() {
-        this.getMain()
+        this.getUsers()
     },
     data() {
         return {
-            userNum: '',
-            postNum: '',
+            fields: [
+                'key',
+                'email',
+                'role',
+                'status',
+                'action'
+            ],
             email: '',
-            list: []
+            list: [],
         }
     },
     methods: {
-        getMain() {
+        getUsers() {
             if (this.$store.state.role === '관리자') {
                 axios
                 .get('http://localhost:8080/admin/users?email='+this.email,
@@ -44,7 +60,28 @@ export default {
             } else {
                 this.$router.push('/');
             }
-        }
+        },
+        deleteUser(id) {
+            const userId = id;
+            console.log(userId);
+            console.log(this.$store.state.accessToken);
+
+            axios
+            .post('http://localhost:8080/users/' + userId + '/delete','',
+            {
+                headers: {
+                    'Authorization' : this.$store.state.accessToken,
+                }
+            })
+            .then((res) => {
+                console.log(res);
+                const item = this.list.find((item) => item.id === userId);
+                item.status = res.data;
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        },
     },
 }
 </script>
@@ -67,5 +104,9 @@ h1 {
 .tbl-wrapper {
     width: 60%;
     margin-left: 20%;
+}
+.btn-del {
+    height: 25px;
+    font-size: 10px;
 }
 </style>
