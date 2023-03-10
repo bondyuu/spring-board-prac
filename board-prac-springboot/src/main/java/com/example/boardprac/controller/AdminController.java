291a6f4c07e425629a3f7db2c49791cd19d1f940
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-03-10 17:40:56 +0900

    Post, User의 Status에 대한 enum 추가로 인한 수정

--- board-prac-springboot/src/main/java/com/example/boardprac/controller/AdminController.java
+++ board-prac-springboot/src/main/java/com/example/boardprac/controller/AdminController.java
@@ -5,6 +5,9 @@ import com.example.boardprac.service.AdminService;
 import com.example.boardprac.service.PostService;
 import com.example.boardprac.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -40,4 +43,10 @@ public class AdminController {
         return postService.getPostsByUser(id, userDetails);
     }
 
+    @GetMapping("/posts")
+    public ResponseEntity<?> getPosts(@RequestParam String title,
+                                      @AuthenticationPrincipal UserDetailsImpl userDetails,
+                                      @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+        return adminService.searchPost(title, userDetails, pageable);
+    }
 }
